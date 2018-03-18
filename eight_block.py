7a390471a4c7244cbb613a1ab854761bd6f824ed
Ross Weijer <ross.weijer@datascopeanalytics.com>
2018-03-18 12:43:33 -0500

    row and column location functions added to base class

--- eight_block.py
+++ eight_block.py
@@ -16,13 +16,14 @@ class eightBlock():
 			random.shuffle(self.valid_vals)
 			self.board_config = self.valid_vals
 		elif isinstance(positions, list):
-			if set(positions) == set(self.valid_vals):
+			if sorted(positions) == self.valid_vals:
 				self.board_config = positions
 			else:
-				v_msg = "positions must only contain 0 through 8"
+				v_msg = "positions must be a permutation of integers 0-8"
 				raise ValueError(v_msg)
 		else:
-			t_msg = "positions must be a list, not {}".format(type(positions))
+			inpt_tp = type(positions).__name__
+			t_msg = "positions must be a list, not {}".format(inpt_tp)
 			raise TypeError(t_msg)
 			
 	def display_board(self):
@@ -31,12 +32,36 @@ class eightBlock():
 		for r in range(len(self.valid_inds)):
 			print(self.board_config[3*r:3*(r+1)])
 
+	def get_row(self, board_val):
+		'''Given a value 0-8, this function will return the row where that 
+		value currently is
+		'''
+		curr_ind = self.board_config.index(board_val)
+		return int(curr_ind/3)
+
+	def get_col(self, board_val):
+		'''Given a value 0-8, this function will return the column where that 
+		value currently is
+		'''
+		curr_ind = self.board_config.index(board_val)
+		return curr_ind % 3
+
 
 if __name__ == "__main__":
-	ipdb.set_trace()
+    
+    foo = eightBlock()
     # Try to display the board properly
+    foo.display_board()
 
+    # Check that you can index into things properly
+    for v in range(len(foo.valid_vals)):
+	    r = foo.get_row(v)
+	    c = foo.get_col(v)
+	    print("{} located at position ({},{})".format(v, r, c))
+    ipdb.set_trace()
+
+    # Test whether the board is solved
 
     # Try to move the empty slot one space
 
-    # Boolean to test whether the board is solved
+    
