3d64e1807e12306bebe19c53174bfd806bf8be9a
Ross Weijer <ross.weijer@datascopeanalytics.com>
2018-04-17 20:07:59 -0500

    hamming distance implemented in baseHeuristicSolver class

--- heuristic.py
+++ heuristic.py
@@ -9,25 +9,26 @@ call different heuristics as well
 
 class baseHeuristicSolver(eightBlock):
 
-	def __init__(self, heuristic, start_state = None, goal_state = None):
+    def __init__(self, heuristic, start_state = None, goal_state = None):
 
-		super().__init__(start_state, goal_state)
-		self.h_dict = {"misplaced_tiles": self.get_misplaced_values,
-					   "manhattan_distance": self.manhattan_distance}
-		if heuristic not in self.h_dict.keys():
-			h_tried = "Tried to use heuristic {}.".format(heuristic)
-			valids = ", ".join([v for v in self.h_dict.keys()])
-			h_valid = "Must be one of {}.".format(valids)
-			e_msg = " ".join([h_tried, h_valid])
-			raise NotImplementedError(e_msg)
+        super().__init__(start_state, goal_state)
+        self.h_dict = {"hamming": self.hamming_distance,
+                       "manhattan": self.manhattan_distance}
+        if heuristic not in self.h_dict.keys():
+            h_tried = "Tried to use heuristic {}.".format(heuristic)
+            valids = ", ".join([v for v in self.h_dict.keys()])
+            h_valid = "Must be one of {}.".format(valids)
+            e_msg = " ".join([h_tried, h_valid])
+            raise NotImplementedError(e_msg)
+        self.calculate_heuristic = self.h_dict[heuristic]
 
+    def hamming_distance(self, board = None):
+        return len(self.get_misplaced_values(board))
 
-
-	def manhattan_distance(self):
-		pass
+    
+            
 
 
 if __name__ == "__main__":
-	print("shoop-a-doop")
-	foo = baseHeuristicSolver("misplaced_tiles")
-	ipdb.set_trace()
+    foo = baseHeuristicSolver("hamming")
+    ipdb.set_trace()
